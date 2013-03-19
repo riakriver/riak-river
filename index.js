3ae393dd042339c4353c3621c8482d4248930bc9
wlaurance <w.laurance@gmail.com>
2013-03-19 19:37:11 -0400

    Express init

--- index.js
+++
@@ -1,6 +0,0 @@
-var tako = require('tako'),
-  app = tako();
-
-app.route('/').json({msg:'riak hosting for the rest of us', extra:'coming soon to a server near you!'});
-
-app.httpServer.listen(80);
