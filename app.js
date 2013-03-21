42be2e54bd5ac980480bcfd33e15aba95705c47e
wlaurance <w.laurance@gmail.com>
2013-03-20 22:58:23 -0400

    WIP
    jQuery
    Socket.io
    
    email form

--- app.js
+++ app.js
@@ -29,6 +29,19 @@ app.configure('development', function(){
 
 app.get('/', routes.index);
 
-http.createServer(app).listen(app.get('port'), function(){
+var server = http.createServer(app).listen(app.get('port'), function(){
   console.log("Express server listening on port " + app.get('port'));
 });
+
+/*
+ * socket.io configuration
+ */
+var io = require('socket.io').listen(server);
+
+io.sockets.on('connection', function(socket){
+  socket.on('new subscriber', function(data){
+    console.log(data);
+  });
+});
+
+app.locals.socketio = process.env.SOCKETIO_HOST || 'http://localhost:' + app.get('port');
