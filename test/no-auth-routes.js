567add3a65cb79f41634aaab4adc6e17791fe030
wlaurance <w.laurance@gmail.com>
2013-07-14 13:03:40 -0400

    Test good routes

--- test/no-auth-routes.js
+++ test/no-auth-routes.js
@@ -2,11 +2,8 @@ var should = require('should'),
     request = require('request'),
     server_helper = require(__dirname + '/server_helper'),
     port = (process.env.TESTING_PORT || 3000),
-    host = (process.env.TESTING_HOST || "http://localhost") + ':' + port;
-
-var isGoodResponse = function(res){
-  res.should.have.property('statusCode', 200);
-}
+    host = (process.env.TESTING_HOST || "http://localhost") + ':' + port,
+    async = require('async');
 
 describe('no authentication required', function(){
   before(function(done){
@@ -16,10 +13,13 @@ describe('no authentication required', function(){
       done();
     }
   });
-  it('should have a route a /', function(done){
-    request(host, function(e,r,b){
-      isGoodResponse(r);
-      done();
-    });
+  it('should have a routes', function(done){
+    var iterator = function(route, callback){
+      request(host + route, function(e,r,b){
+        r.should.have.property('statusCode', 200);
+        callback();
+      });
+    }
+    async.each(['/', '/about', '/contact', '/login'], iterator, done);
   });
 });
