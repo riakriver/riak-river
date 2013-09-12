fc73b9e6622e81d9544dc981f28b3d26a34e4511
wlaurance <w.laurance@gmail.com>
2013-09-11 21:13:32 -0400

    Restart

--- routes/account.js
+++
@@ -1,7 +0,0 @@
-var m = require(__dirname + '/../middleware');
-module.exports = function(app){
-  app.get('/account', m.requireAuth, function(req, res){
-    console.log(req.user);
-    res.render('account', {user:req.user});
-  });
-}
